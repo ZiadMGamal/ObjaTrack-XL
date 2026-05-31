32c83741d12564bbd5f77ebfbc8b987d2a3ec623
Ziad Mohamed Gamal Mahmoud <ziadmgamal.ai@gmail.com>
2026-05-31 17:45:04 +0300

    Update in The Init Optimization

--- src/optimization/__init__.py
+++ src/optimization/__init__.py
@@ -3,3 +3,13 @@ from src.optimization.onnx_exporter import ONNXExporter
 from src.optimization.quantizer import ModelQuantizer
 from src.optimization.model_validator import ModelValidator
 from src.optimization.profiler import ModelProfiler
+from src.optimization.trt_exporter import TensorRTExporter
+
+__all__ = [
+    "BaseModelOptimizer",
+    "ONNXExporter",
+    "ModelQuantizer",
+    "ModelValidator",
+    "ModelProfiler",
+    "TensorRTExporter",
+]
