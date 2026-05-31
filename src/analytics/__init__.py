524b3be8eec79428138eb7892ad7f531d142b740
Ziad Mohamed Gamal Mahmoud <ziadmgamal.ai@gmail.com>
2026-05-31 17:06:54 +0300

    Init

--- src/analytics/__init__.py
+++ src/analytics/__init__.py
@@ -1,5 +1,15 @@
 from src.analytics.counter import ObjectCounter
-from src.analytics.zone import ZoneAnalytics
+from src.analytics.zone import Zone, ZoneAnalytics
 from src.analytics.speed_estimator import SpeedEstimator
 from src.analytics.dwell_time import DwellTimeAnalyzer
-from src.analytics.event_detector import EventDetector
+from src.analytics.event_detector import EventDetector, EventType
+
+__all__ = [
+    "ObjectCounter",
+    "Zone",
+    "ZoneAnalytics",
+    "SpeedEstimator",
+    "DwellTimeAnalyzer",
+    "EventDetector",
+    "EventType",
+]
