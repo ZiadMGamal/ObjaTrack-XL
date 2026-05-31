a5ea833e038136aedc4a982031586843aea99464
Ziad Mohamed Gamal Mahmoud <ziadmgamal.ai@gmail.com>
2026-05-31 17:10:23 +0300

    __init__ Detection

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -1,4 +1,20 @@
 from src.detection.base_detector import BaseObjectDetector
 from src.detection.yolo_detector import YOLODetector
 from src.detection.onnx_detector import ONNXDetector
+from src.detection.trt_detector import TensorRTDetector
 from src.detection.detector_factory import DetectorFactory
+from src.detection.nms import non_max_suppression, soft_nms, batched_nms, diou_nms
+from src.detection.preprocessing import DetectionPreprocessor
+
+__all__ = [
+    "BaseObjectDetector",
+    "YOLODetector",
+    "ONNXDetector",
+    "TensorRTDetector",
+    "DetectorFactory",
+    "DetectionPreprocessor",
+    "non_max_suppression",
+    "soft_nms",
+    "batched_nms",
+    "diou_nms",
+]
